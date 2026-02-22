8f9f73badafa1bcf97f77eb8983248ccd8deb447
Praveen Thanikachalam <100035228+prave01@users.noreply.github.com>
2026-02-22 13:57:12 +0530

    updates VideoModal.tsx

--- components/custom/molecule/VideoModal.tsx
+++ components/custom/molecule/VideoModal.tsx
@@ -43,21 +43,22 @@ export default function VideoModal({
   return (
     <div
       className={cn(
-        "absolute z-40 w-full h-screen",
-        "items-center justify-center inset-0",
+        "fixed z-50 w-full h-full",
+        "items-center justify-center inset-y-0",
         open ? "flex" : "hidden",
       )}
     >
       <div
         onClick={() => setOpen(false)}
-        className="absolute backdrop-blur-md bg-black/80 w-full h-full -z-10"
+        className="absolute backdrop-blur-md bg-black/80 w-full h-full top-0
+          -z-10"
       />
 
       <video
         ref={videoRef}
         src={source}
         controls
-        className="w-380 h-180 rounded-2xl"
+        className="w-380 h-auto lg:h-180 rounded-none lg:rounded-2xl"
       />
     </div>
   );
